0086ac5834365ef48bb11dc147d426eedafd72f9
Isaias Santos <isaiassousa032@gmail.com>
2023-09-01 15:14:46 -0300

    feat (database) : criação da classe BethanysPieShopDbContext

--- BethanysPieShop/Models/BethanysPieShopDbContext.cs
+++ BethanysPieShop/Models/BethanysPieShopDbContext.cs
@@ -1,6 +1,14 @@
-﻿namespace BethanysPieShop.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BethanysPieShop.Models
 {
-    public class BethanysPieShopDbContext
+    public class BethanysPieShopDbContext: DbContext
     {
+        public BethanysPieShopDbContext(DbContextOptions<BethanysPieShopDbContext> options) : base(options) 
+        { 
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Pie> Pies { get; set; }
     }
 }
