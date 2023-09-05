d7bd123b0815599a6d97109e9eeb46ebf9f638d7
Isaias Santos <isaiassousa032@gmail.com>
2023-09-05 16:57:51 -0300

    refactor : recria o formulário usando Razor Pages

--- BethanysPieShop/Program.cs
+++ BethanysPieShop/Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddSession();
 builder.Services.AddHttpContextAccessor();
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 builder.Services.AddDbContext<BethanysPieShopDbContext>(options => {
     options.UseSqlServer(
         builder.Configuration["ConnectionStrings:BethanysPieShopDbContextConnection"]);
@@ -28,5 +29,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.MapDefaultControllerRoute();
+app.MapRazorPages();
 DbInitializer.Seed(app);
 app.Run();
